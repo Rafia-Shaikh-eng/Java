e7375e6acb8f559de6112cbc12958ca5da1df9ba
Rafia Shaikh <64796657+Rafia-Shaikh-eng@users.noreply.github.com>
2022-11-20 10:04:53 +0000

    Pending changes exported from your codespace

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,18 @@
 
 package com.mycompany.app;
 
+import javax.print.event.PrintEvent;
+
 public class App {
+    public static void Say(){
+        System.out.println("Hi");
+    }
     public static void main(String[] args) {
+        int a=1;
+        float b=2.1f;
         System.out.println("Hello Remote World!");
+        System.out.println(a+b);
+        Say();
+
     }
 }
